63950efe8837de158657527473aedb106b47925f
vamsiforvlsi <circuitgamers1234@gmail.com>
2025-09-03 00:26:04 +0530

    Create inverter_synth.v

---
+++ ASIC/BASICS/Inverter/D.Synthesis/inverter_synth.v
@@ -0,0 +1,12 @@
+module inv(y, x);
+  (* src = "inverter/inverter.v:1.27-1.28" *)
+  input x;
+  wire x;
+  (* src = "inverter/inverter.v:1.19-1.20" *)
+  output y;
+  wire y;
+  INV_X1 _0_ (
+    .A(x),
+    .ZN(y)
+  );
+endmodule
