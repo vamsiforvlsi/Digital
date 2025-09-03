7fba698c932eadb2978310915dab29a0566aa7c0
vamsiforvlsi <circuitgamers1234@gmail.com>
2025-09-03 22:31:08 +0530

    Create detailed_placement.tcl

---
+++ ASIC/BASICS/Inverter/H.Placement/Detailed_placement/detailed_placement.tcl
@@ -0,0 +1,14 @@
+# Detailed Placement
+
+detailed_placement
+
+# Capture utilization before fillers make it 100%
+utl::metric "DPL::utilization" [format %.1f [expr [rsz::utilization] * 100]]
+utl::metric "DPL::design_area" [sta::format_area [rsz::design_area] 0]
+
+# checkpoint
+set dpl_db [make_result_file ${design}_${platform}_dpl.db]
+write_db $dpl_db
+
+set verilog_file [make_result_file ${design}_${platform}.v]
+write_verilog $verilog_file
